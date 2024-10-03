adfd7155421e1536bb6c8102bcb4e50cc62db4bd
josep.valcarce.girbau <josep.valcarce.girbau@auleseebe.upc.edu>
2024-10-03 13:25:22 +0200

    canvis exercici 1

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,15 @@
 #Codigo para problema 1
 
+# coeficient de determinació R^2
+#coeficient de correlació R
+#et donen el que li dona R al fer la funció lm trobar el que et demanen en el mateix enunciat
+# coeficient de determinació R^2 = 0,1923
+# coeficient de correlació = sqrt(0,1923)
+R=sqrt(0.1923)
+R
+#variació del valor de mercat per un augment dels actius, x=actius, y=valor de mercat
+m=0.06576  # en el summary de lm surt com el nom que li hagis posat a la x i es el valor de la primera columna
+b=11.79755 #en el summary surt com a intercepció
+x=647.1
+y=x*m+b
+y
